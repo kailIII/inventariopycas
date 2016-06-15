14be21a9b1e546fd6f5744a36d333337e4d0a9ac
Sergio Renaato Mendoza Pisconte <renato.mpisconte@gmail.com>
2016-06-15 17:46:11 -0500

    Iniciando nuevo proyecto

---
+++ application/views/person_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    </head> 
+<body>
+    <h1>Iniciando</h1>
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+
+});
+</script>
+</body>
+</html>
